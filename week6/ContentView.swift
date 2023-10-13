1c1ba3881f5314d22ab91f85967f8a6badc7012e
Giselle <giselle.mingue@gmail.com>
2023-10-13 13:09:43 -0400

    Initial Commit

---
+++ week6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  week6
+//
+//  Created by Giselle Mingue Rios on 2023-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ week6/week6App.swift
@@ -0,0 +1,17 @@
+//
+//  week6App.swift
+//  week6
+//
+//  Created by Giselle Mingue Rios on 2023-10-13.
+//
+
+import SwiftUI
+
+@main
+struct week6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
