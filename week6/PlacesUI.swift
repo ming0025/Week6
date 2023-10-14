2c6a9632bdabd8f1a4ef30564b7d48a9fbebae26
Giselle Mingue <ming0025@algonquinlive.com>
2023-10-14 16:31:11 -0400

    Adding changes

---
+++ week6/PlacesUI.swift
@@ -0,0 +1,18 @@
+//
+//  PlacesUI.swift
+//  week6
+//
+//  Created by Giselle Mingue Rios on 2023-10-13.
+//
+
+import SwiftUI
+
+struct PlacesUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlacesUI()
+}
